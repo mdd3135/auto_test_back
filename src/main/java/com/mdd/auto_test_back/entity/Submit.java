c55de0001181a2220eca2750c54b23befa158f59
mdd3135 <313521996@qq.com>
2024-02-15 23:37:02 +0800

    修改submit类

--- src/main/java/com/mdd/auto_test_back/entity/Submit.java
+++ src/main/java/com/mdd/auto_test_back/entity/Submit.java
@@ -4,11 +4,31 @@ public class Submit {
     int id;
     int homeworkId;
     int userId;
+    int itemId;
+    int type;
 
-    public Submit(int id, int homeworkId, int userId) {
+    public Submit(int id, int homeworkId, int userId, int itemId, int type) {
         this.id = id;
         this.homeworkId = homeworkId;
         this.userId = userId;
+        this.itemId = itemId;
+        this.type = type;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
     }
 
     public int getId() {
